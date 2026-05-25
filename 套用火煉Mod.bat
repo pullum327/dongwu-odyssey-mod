452a8cc71134c00a0f72a18809e05133f6386952
pullum327 <91268908+pullum327@users.noreply.github.com>
2026-05-25 13:08:57 +0800

    Delete 套用火煉Mod.bat

--- 套用火煉Mod.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0.."
-echo 請先關閉遊戲，再按任意鍵套用修補...
-pause >nul
-python "_ignite_mod\apply_mods.py" --only ignite_no_consume,ignite_changming_triple
-if errorlevel 1 pause
-else echo 完成。
-pause
